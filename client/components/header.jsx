36bbf38e208095fcb625fdf89107c0c32c2f6199
Michael Zhu <mzhu23@gmail.com>
2020-04-24 10:24:45 +0000

    Defined Header component & added to App's render method

---
+++ client/components/header.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Header() {
+  return <h1 className="p-3 mb-2 bg-dark text-white"><i className="fas fa-dollar-sign mr-2"></i>Wicked Sales</h1>;
+}
