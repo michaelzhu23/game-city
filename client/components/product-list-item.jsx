4db0c8fe0262c31fd504a230441eefd5a10eb330
Michael Zhu <mzhu23@gmail.com>
2020-04-24 18:57:23 +0000

    Defined ProductListItem component that renders a single product card

---
+++ client/components/product-list-item.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function ProductListItem() {
+  return (
+    <div className="card" style="width: 18rem;">
+      <img src="" className="card-img-top" alt=""/>
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <h6 className="card-subtitle mb-2 text-muted">Price</h6>
+        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card content.</p>
+      </div>
+    </div>
+  );
+}
