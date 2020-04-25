ef78684ac5737037ae9b2dbcf883763104a048f1
Michael Zhu <mzhu23@gmail.com>
2020-04-25 06:22:44 +0000

    Defined ProductDetails component

---
+++ client/components/product-details.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class ProductDetails extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { product: null };
+  }
+
+  render() {
+    return null;
+  }
+}
