3e37d9043b57e97c71e1fdb8cb289c4ea9aaa200
Michael Zhu <mzhu23@gmail.com>
2020-04-25 10:45:44 +0000

    Completed ProductDetails component's render method to conditionally render view of a product's details

--- client/components/product-details.jsx
+++ client/components/product-details.jsx
@@ -11,10 +11,34 @@ export default class ProductDetails extends React.Component {
   componentDidMount() {
     fetch('/api/products/1')
       .then(response => response.json())
-      .then(data => console.log(data));
+      .then(data => this.setState({ product: data }));
   }
 
   render() {
+    if (this.state.product) {
+      return (
+        <div className="card details-card">
+          <div className="col-md-12 m-4">
+            <p className="text-secondary">&#60; Back to catalog</p>
+          </div>
+          <div className="row no-gutters">
+            <div className="col-md-5 pl-3">
+              <img src={`${this.state.product.image}`} className="card-img img-size-detail" alt=""/>
+            </div>
+            <div className="col-md-7">
+              <div className="card-body">
+                <h2 className="card-title mb-3">{this.state.product.name}</h2>
+                <h5 className="card-subtitle mb-2 text-muted">{'$' + (this.state.product.price / 100).toFixed(2)}</h5>
+                <p className="card-text">{this.state.product.shortDescription}</p>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-12 my-3">
+            <p>{this.state.product.longDescription}</p>
+          </div>
+        </div>
+      );
+    }
     return null;
   }
 }
