4fe46a1b06677620e4891dc2e27c6417d7bdab0f
Michael Zhu <mzhu23@gmail.com>
2020-04-25 23:15:43 +0000

    Added initial state to App component

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -3,6 +3,16 @@ import Header from './header';
 import ProductList from './product-list';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      view: {
+        name: 'catalog',
+        params: {}
+      }
+    };
+  }
+
   render() {
     return (
       <>
