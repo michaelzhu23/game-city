27b6dd8370dfb61e9f985fb5131cef3716e3fed0
Michael Zhu <mzhu23@gmail.com>
2020-04-25 07:16:40 +0000

    Added componentDidMount method to ProductDetails component that sends GET request to retrieve product details by productId

--- client/components/product-details.jsx
+++ client/components/product-details.jsx
@@ -1,9 +1,17 @@
+/* eslint-disable no-console */
+
 import React from 'react';
 
 export default class ProductDetails extends React.Component {
   constructor(props) {
     super(props);
     this.state = { product: null };
+  }
+
+  componentDidMount() {
+    fetch('/api/products/1')
+      .then(response => response.json())
+      .then(data => console.log(data));
   }
 
   render() {
