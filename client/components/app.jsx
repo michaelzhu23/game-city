004cde54d44706936f717b1ca1dc2090feba9cf2
Michael Zhu <mzhu23@gmail.com>
2020-04-29 08:28:41 +0000

    Added an initial state property to App component named cart

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -7,6 +7,7 @@ export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      cart: [],
       view: {
         name: 'catalog',
         params: {}
