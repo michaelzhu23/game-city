80884eb68bf9c33e189f714795b307c03fba0d0b
Michael Zhu <mzhu23@gmail.com>
2020-05-22 01:45:42 +0000

    Defined stateful CheckoutForm component within checkout-form.jsx

---
+++ client/components/checkout-form.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default class CheckoutForm extends React.component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      creditCard: '',
+      address: ''
+    };
+  }
+
+  render() {
+
+  }
+}
