59443b740f60c6dc3d58358c602bef71825c2013
Michael Zhu <mzhu23@gmail.com>
2020-05-23 08:20:59 +0000

    Modified ProductDetails component to wrap all react elements in a 'container' div

--- client/components/product-details.jsx
+++ client/components/product-details.jsx
@@ -17,36 +17,38 @@ export default class ProductDetails extends React.Component {
   render() {
     if (this.state.product) {
       return (
-        <div className="card details-card ">
-          <div className="col-md-12 my-4">
-            <p
-              className="text-secondary d-inline on-hover"
-              onClick={() => {
-                this.props.setProductView('catalog', {});
-              }}>
+        <div className="container">
+          <div className="card details-card ">
+            <div className="col-md-12 my-4">
+              <p
+                className="text-secondary d-inline on-hover"
+                onClick={() => {
+                  this.props.setProductView('catalog', {});
+                }}>
               &#60; Back to catalog
-            </p>
-          </div>
-          <div className="row no-gutters">
-            <div className="col-md-5 pl-3">
-              <img src={`${this.state.product.image}`} className="card-img img-size-detail" alt=""/>
+              </p>
             </div>
-            <div className="col-md-7">
-              <div className="card-body">
-                <h2 className="card-title mb-3">{this.state.product.name}</h2>
-                <h5 className="card-subtitle mb-2 text-muted">{'$' + (this.state.product.price / 100).toFixed(2)}</h5>
-                <p className="card-text">{this.state.product.shortDescription}</p>
-                <button
-                  onClick={() => this.props.addProductToCart(this.props.viewParamsState)}
-                  type="button"
-                  className="btn btn-primary">
+            <div className="row no-gutters">
+              <div className="col-md-5 pl-3">
+                <img src={`${this.state.product.image}`} className="card-img img-size-detail" alt=""/>
+              </div>
+              <div className="col-md-7">
+                <div className="card-body">
+                  <h2 className="card-title mb-3">{this.state.product.name}</h2>
+                  <h5 className="card-subtitle mb-2 text-muted">{'$' + (this.state.product.price / 100).toFixed(2)}</h5>
+                  <p className="card-text">{this.state.product.shortDescription}</p>
+                  <button
+                    onClick={() => this.props.addProductToCart(this.props.viewParamsState)}
+                    type="button"
+                    className="btn btn-primary">
                   Add to Cart
-                </button>
+                  </button>
+                </div>
               </div>
             </div>
-          </div>
-          <div className="col-md-12 my-3">
-            <p>{this.state.product.longDescription}</p>
+            <div className="col-md-12 my-3">
+              <p>{this.state.product.longDescription}</p>
+            </div>
           </div>
         </div>
       );
