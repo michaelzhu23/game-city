a4962564d387616e2a03e83ea78dc0d9b4e1c69d
Michael Zhu <michaelzhu@Michaels-MBP.home>
2020-06-17 01:15:44 -0700

    Added states using useState hooks & added ConfirmRemoveModal component to return statement & defined handleOpenModal method and passed to CartSummaryItem via props

--- client/components/cart-summary.jsx
+++ client/components/cart-summary.jsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import CartSummaryItem from './cart-summary-item';
+import ConfirmRemoveModal from './confirm-remove-modal';
 
 export default function CartSummary(props) {
   let cartItems;
+  const [open, setOpen] = useState(false);
+  const [cartItemToRemove, setCartItemToRemove] = useState({ name: '', image: '' });
+  const handleOpenModal = cartItem => {
+    setOpen(true);
+    setCartItemToRemove(cartItem);
+  };
   if (!props.cartItems.length) {
     cartItems = <h2 className="text-danger mb-5">Your cart is empty!</h2>;
   } else {
@@ -11,7 +18,7 @@ export default function CartSummary(props) {
         <CartSummaryItem
           key={cartItem.productId}
           cartItem={cartItem}
-          removeFromCart={props.removeFromCart}
+          handleOpenModal = {handleOpenModal}
         />
       );
     });
@@ -33,20 +40,27 @@ export default function CartSummary(props) {
     );
   }
   return (
-    <div className="container">
-      <div className="col-md-12 mb-4 p-0">
-        <p
-          className="text-gray d-inline on-hover"
-          onClick={() => props.setView('catalog', {})}>
-          <i className="fas fa-chevron-circle-left fa-lg mr-1"></i> Continue Shopping
-        </p>
+    <>
+      <div className="container">
+        <div className="col-md-12 mb-4 p-0">
+          <p
+            className="text-gray d-inline on-hover"
+            onClick={() => props.setView('catalog', {})}>
+            <i className="fas fa-chevron-circle-left fa-lg mr-1"></i> Continue Shopping
+          </p>
+        </div>
+        <h1 className="mb-5">My Cart</h1>
+        {cartItems}
+        <div className="d-flex justify-content-between align-items-baseline">
+          <h3 className="mt-3">Item Total ${(totalPrice / 100).toFixed(2)}</h3>
+          {checkoutButton}
+        </div>
       </div>
-      <h1 className="mb-5">My Cart</h1>
-      {cartItems}
-      <div className="d-flex justify-content-between align-items-baseline">
-        <h3 className="mt-3">Item Total ${(totalPrice / 100).toFixed(2)}</h3>
-        {checkoutButton}
-      </div>
-    </div>
+      <ConfirmRemoveModal
+        show={open}
+        cartItem={cartItemToRemove}
+        handleCloseModal={() => setOpen(false)}
+        removeFromCart={props.removeFromCart} />
+    </>
   );
 }
