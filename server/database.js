2e2775ff797323ca84e8cb530e4223ecf32e7444
Michael Zhu <mzhu23@gmail.com>
2026-06-29 23:56:53 -0700

    Enable SSL for Supabase Postgres in production

--- server/database.js
+++ server/database.js
@@ -1,7 +1,10 @@
 const pg = require('pg');
 
 const db = new pg.Pool({
-  connectionString: process.env.DATABASE_URL
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === 'production'
+    ? { rejectUnauthorized: false }
+    : undefined
 });
 
 module.exports = db;
