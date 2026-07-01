6c2e79fc33e7e057a1e4ecce5d4d0a31e3e32053
Michael Zhu <mzhu23@gmail.com>
2026-06-30 19:03:46 -0700

    Normalize Supabase connection string for Render

--- server/database.js
+++ server/database.js
@@ -1,13 +1,29 @@
+const dns = require('dns');
 const pg = require('pg');
 
-const connectionString = process.env.DATABASE_URL;
+if (dns.setDefaultResultOrder) {
+  dns.setDefaultResultOrder('ipv4first');
+}
+
+function buildConnectionString() {
+  const connectionString = process.env.DATABASE_URL;
+  if (!connectionString) return connectionString;
+
+  const url = new URL(connectionString);
+  if (!url.searchParams.has('sslmode')) {
+    url.searchParams.set('sslmode', 'require');
+  }
+  return url.toString();
+}
+
+const connectionString = buildConnectionString();
 
 const db = new pg.Pool({
   connectionString,
   ssl: process.env.NODE_ENV === 'production'
     ? { rejectUnauthorized: false }
     : undefined,
-  connectionTimeoutMillis: 10000
+  connectionTimeoutMillis: 30000
 });
 
 module.exports = db;
